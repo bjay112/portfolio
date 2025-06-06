02386fc6124324a4f98f8047308cc18b00c6b5d1
bj537 <bjeleazar922@gmail.com>
2025-06-06 14:49:59 +0800

    Initial commit

---
+++ components/data.js
@@ -0,0 +1,66 @@
+// Sample photo data
+const photos = [
+    {
+        id: 1,
+        title: "Portrait 1",
+        imageUrl: "/images/1bdadb7b-49da-4a7b-bbb1-12ddb21de048.jpg",
+        featured: true
+    },
+    {
+        id: 2,
+        title: "Portrait 2",
+        imageUrl: "/images/1eec5d81-5c5b-4d35-8c29-7b1be37d8992.jpg",
+        featured: true
+    },
+    {
+        id: 3,
+        title: "Portrait 3",
+        imageUrl: "/images/6d3843f7-55a2-4518-b4fa-fbd382aecd5d.jpg",
+        featured: true
+    },
+    {
+        id: 4,
+        title: "Portrait 4",
+        imageUrl: "/images/9fdf2ba4-da15-44d5-88b2-00f621cbd57c.jpg",
+        featured: true
+    },
+    {
+        id: 5,
+        title: "Portrait 5",
+        imageUrl: "/images/73b5ec68-4f57-48de-89af-ec23a2a59c12.jpg",
+        featured: true
+    },
+    {
+        id: 6,
+        title: "Portrait 6",
+        imageUrl: "/images/3118be66-8028-443f-b1e5-e37001c7a392.jpg",
+        featured: true
+    },
+    {
+        id: 7,
+        title: "Portrait 7",
+        imageUrl: "/images/20104e69-292c-48a1-bc34-fa453b9f5d90.jpg",
+        featured: true
+    },
+    {
+        id: 8,
+        title: "Portrait 8",
+        imageUrl: "/images/248403f3-afb5-4e28-bd07-9f2cad4ddfbe.jpg",
+        featured: true
+    },
+    {
+        id: 9,
+        title: "Portrait 9",
+        imageUrl: "/images/a11db988-9d56-4ed0-beb7-057402f0825e.jpg",
+        featured: true
+    },
+    {
+        id: 10,
+        title: "Portrait 10",
+        imageUrl: "/images/f0561402-c5fe-4db7-83f0-ff80ef18fe9c.jpg",
+        featured: true
+    }
+];
+
+// Categories for filtering
+const categories = ['All', 'Portrait']; 
